b2f47ec90dcd92c9a57f912f901feb8632b9c356
cobi <cobi@cb14c9df-8a02-4fc1-9b40-3ccfc4edb2c1>
2010-01-05 08:13:53 +0000

    Let's see if this works.
    git-svn-id: https://cobihome.external.cluenet.org:8443/svn/phpserv/trunk@27 cb14c9df-8a02-4fc1-9b40-3ccfc4edb2c1

--- modules/services/nickserv.php
+++ modules/services/nickserv.php
@@ -2,6 +2,27 @@
 	class nickserv {
 		function construct () {
 			$this->event_eos('a');
+			if(!ismod('commandutils'))
+				load('modules/misc/commandutils.php');
+		}
+		
+		function event_commandutils_load() {
+			if(!ismod('commandutils')) {
+				logit('Huh?  We were told commandutils was ready, but it does not exist.');
+				return;
+			}
+				
+			getmod('commandutils')->registercommand($this, 'anon', 'IDENTIFY', '<password>[:<username>] - Identifies you to services.');
+			getmod('commandutils')->registercommand($this, 'anon', 'IDENT', 'Alias for IDENTIFY.');
+			getmod('commandutils')->registercommand($this, 'anon', 'ID', 'Alias for IDENTIFY.');
+			
+			getmod('commandutils')->registercommand($this, 'auth', 'IDENTIFY', '<password>[:<username>] - Identifies you to services.');
+			getmod('commandutils')->registercommand($this, 'auth', 'IDENT', 'Alias for IDENTIFY.');
+			getmod('commandutils')->registercommand($this, 'auth', 'ID', 'Alias for IDENTIFY.');
+			
+			getmod('commandutils')->registercommand($this, 'auth', 'REGISTER', 'Associates your current nick with your PHPServ account.');
+			getmod('commandutils')->registercommand($this, 'auth', 'DROP', 'Dissociates your current nick with your PHPServ account.');
+			getmod('commandutils')->registercommand($this, 'auth', 'GHOST', '<nick> - Kills <nick> if you are the owner of it.');
 		}
 
 		function destruct () {
@@ -9,6 +30,80 @@
 			$ircd->quit('NickServ','La Gone!');
 		}
 
+		function command_anon_id ($from,$to,$rest,$extra) {
+			$this->command_anon_identify($from,$to,$rest,$extra);
+		}
+		
+		function command_anon_ident ($from,$to,$rest,$extra) {
+			$this->command_anon_identify($from,$to,$rest,$extra);
+		}
+		
+		function command_anon_identify ($from,$to,$rest,$extra) {
+			$p = explode(':',$rest[0]);
+
+			if (isset($p[1])) {
+				event('msg',$from,'PHPServ','IDENTIFY '.$p[1].' '.$p[0]);
+			} else {
+				event('msg',$from,'PHPServ','IDENTIFY '.$from.' '.$p[0]);
+			}
+		}
+		
+		function command_auth_id ($from,$to,$rest,$extra) {
+			$this->command_auth_identify($from,$to,$rest,$extra);
+		}
+		
+		function command_auth_ident ($from,$to,$rest,$extra) {
+			$this->command_auth_identify($from,$to,$rest,$extra);
+		}
+		
+		function command_auth_identify ($from,$to,$rest,$extra) {
+			$ircd = &ircd();
+			
+			$ircd->notice($to,$from,'You are already identified!');
+		}
+		
+		function command_auth_register ($from,$to,$rest,$extra) {
+			global $mysql;
+			$ircd = &ircd();
+			
+			if ($mysql->get($mysql->sql('SELECT * FROM `nickserv` WHERE `nick` = '.$mysql->escape($from))))
+				$ircd->notice($to,$from,'Your nick is already owned by someone else.');
+			else {
+				$data = array
+				(
+					'id'		=> 'NULL',
+					'userid'	=> $extra['uid'],
+					'nick'		=> $from
+				);
+				$mysql->insert('nickserv',$data);
+				$ircd->notice($to,$from,'"'.$from.'" is now linked to your PHPServ Account.');
+			}
+		}
+		
+		function command_auth_ghost ($from,$to,$rest,$extra) {
+			global $mysql;
+			$ircd = &ircd();
+			
+			$data = $mysql->get($mysql->sql('SELECT * FROM `nickserv` WHERE `nick` = '.$mysql->escape($rest[0])));
+			if ($extra['uid'] == $data['userid']) {
+				$ircd->svskill($rest[0],'Connection reset by Ghost. This user has been ghostified by '.$from);
+				$ircd->notice($to,$from,'Ghost busted. :D');
+			} else
+				$ircd->notice($to,$from,'You don\'t own that nick!');
+		}
+		
+		function command_auth_drop ($from,$to,$rest,$extra) {
+			global $mysql;
+			$ircd = &ircd();			
+			
+			$data = $mysql->get($mysql->sql('SELECT * FROM `nickserv` WHERE `nick` = '.$mysql->escape($from)));
+			if ($extra['uid'] == $data['userid']) {
+				$mysql->sql('DELETE FROM `nickserv` WHERE `nick` = '.$mysql->escape($from));
+				$ircd->notice($to,$from,'Nick '.$from.' dropped.');
+			} else
+				$ircd->notice($to,$from,'You don\'t own that nick!');
+		}
+		
 		function event_msg ($from,$to,$message) {
 			global $mysql;
 			$ircd = &ircd();
@@ -16,108 +111,11 @@
 			if ((strtolower($to) == 'nickserv') or (strtolower($to) == 'nickserv@'.strtolower($mysql->getsetting('server')))) {
 				$to = explode('@', $to, 2);
 				$to = $to[0];
-				$d = explode(' ', $message);
 				
 				$nickd = $mysql->get($mysql->sql('SELECT * FROM `users` WHERE `nick` = '.$mysql->escape($from)));
 				$uid = $nickd['loggedin'];
-
-				$command = strtolower($d[0]);
-
-				// "Don't care" commands.
-
-				switch ($command) {
-					case 'help':
-						$ircd->notice($to,$from,'--- Commands overview ---');
-						$ircd->notice($to,$from,'REGISTER                    - Link your nick to your PHPserv account.');
-						$ircd->notice($to,$from,'IDENTIFY <password>[:user]  - Identify to your PHPserv account.');
-						$ircd->notice($to,$from,'GHOST <user>                - Kill a user on your nickname.');
-//						$ircd->notice($to,$from,'INFO [user] [nick|account]  - Retrieve info on a user.');
-						$ircd->notice($to,$from,'DROP                        - Unlink your nick from your PHPserv account.');
-						$ircd->notice($to,$from,'--- End of help ---');
-						return 1;
-					case 'info':
-/*						$who = (isset($d[1]) ? $d[1] : $from)
-						if (isset($d[2]) {
-							switch (strtolower($d[2])) {
-								case 'account':	$what = 'a'; break;
-								case 'nick': $what = 'n'; break;
-								default: $ircd->notice('NickServ',$from,'Invalid syntax. Try "/NS HELP INFO" instead.'); return 0;
-							}
-						} else {
-							$what = 'n';
-						}
-						// Okay, now we've found what we want to get. Let's get them.
-						switch ($what) {
-							case 'a':
-						$data = $mysql->get($mysql->sql('SELECT `user` FROM `access` WHERE `user` = '.$mysql->escape($d[1])))
-						*/
-						break;
-					case 'id':
-					case 'ident':
-					case 'identify':
-						if ($uid == -1) {
-							$p = explode(':',$d[1]);
-
-							if (isset($p[1])) {
-								event('msg',$from,'PHPServ','IDENTIFY '.$p[1].' '.$p[0]);
-							} else {
-								event('msg',$from,'PHPServ','IDENTIFY '.$from.' '.$p[0]);
-							}
-						} else {
-							$ircd->notice('NickServ',$from,'You\'re already identified!');
-						}
-						return 1;
-				}
-
-				if ($uid != -1) {
-				// They're identified. Have fun.
-				switch ($command) {
-					case 'register':
-						if ($mysql->get($mysql->sql('SELECT * FROM `nickserv` WHERE `nick` = '.$mysql->escape($from)))) {
-							$ircd->notice($to,$from,'Your nick is already owned by someone else.');
-							return 0;
-						} else {
-							$data = array
-							(
-								'id'		=> 'NULL',
-								'userid'	=> $uid,
-								'nick'		=> $from
-							);
-							$mysql->insert('nickserv',$data);
-							$ircd->notice($to,$from,'"'.$from.'" is now linked to your PHPServ Account.');
-							return 1;
-						}
-					case 'recover':
-						// Recover is a temporary alias to ghost until someone implements SVSHOLD stuff
-					case 'ghost':
-						$data = $mysql->get($mysql->sql('SELECT * FROM `nickserv` WHERE `nick` = '.$mysql->escape($d[1])));
-						if ($uid == $data['userid']) {
-							$ircd->svskill($d[1],'Connection reset by Ghost. This user has been ghostified by '.$from);
-							$ircd->notice($to,$from,'Ghost busted. :D');
-							return 1;
-						} else {
-							$ircd->notice($to,$from,'You don\'t own that nick!');
-							return 0;
-						}
-					case 'drop':
-						$data = $mysql->get($mysql->sql('SELECT * FROM `nickserv` WHERE `nick` = '.$mysql->escape($from)));
-						if ($uid == $data['userid']) {
-							$mysql->sql('DELETE FROM `nickserv` WHERE `nick` = '.$mysql->escape($from));
-							$ircd->notice($to,$from,'Nick '.$from.' dropped.');
-							return 1;
-						} else {
-							$ircd->notice($to,$from,'You don\'t own that nick!');
-							return 0;
-						}
-					}
-				} else {
-					// Unidentified, tell them
-					$ircd->notice($to,$from,'You\'re not identified to PHPserv!');
-					return 0;
-				}
-
-			// Didn't catch a command or we failed to return, claim unknown command
-			$ircd->notice('NickServ',$from,'Unknown command "'.$d[0].'". Try "/msg NickServ HELP" instead.');
+				
+				getmod('commandutils')->parsecommand($this, $uid == -1 ? 'anon' : 'auth', $from, $to, $message, array('uid' => $uid,'nickd' => $nickd));
 			}
 		}
 
