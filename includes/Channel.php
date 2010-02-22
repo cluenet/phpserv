4db043c0c16a1859dd1299bb6cfa39c61c138c1c
snofox <snofox@cb14c9df-8a02-4fc1-9b40-3ccfc4edb2c1>
2010-02-22 01:38:01 +0000

    Helps if you commit work you wrote... Started channel object
    git-svn-id: https://cobihome.external.cluenet.org:8443/svn/phpserv/trunk@206 cb14c9df-8a02-4fc1-9b40-3ccfc4edb2c1

---
+++ includes/Channel.php
@@ -0,0 +1,78 @@
+<?PHP
+	class Channel {
+		protected $id; // channel ID
+		protected $name; // channel name
+		protected $modes; // simple modes
+		protected $topic; // topic stuff
+		
+		public static function newFromId( $id ) {
+			return self::newFrom( 'id', $id );
+		}
+		
+		public static function newFromName( $nick ) {
+			return self::newFrom( 'nick', $user );
+		}
+		
+		protected static function newFrom( $field, $value ) {
+			$chanData = MySQL::get( MySQL::sql( 'SELECT * FROM `channels` WHERE ' . $field . ' = ' . MySQL::escape( $value ) ) );
+			
+			if( $chanData ) {
+				return new self(
+					$chanData[ 'chanid' ],
+					$chanData[ 'name' ],
+					$chanData[ 'topic' ],
+					$chanData[ 'modes' ],
+				);
+			}
+			
+			return null;
+		}
+		
+		protected static function __construct( $id, $name, $modes, $topic ) {
+			$this->id = $id;
+			$this->name = $name;
+			$this->modes = $modes;
+			$this->topic = $topic;
+		}
+		
+		protected static function __set( $name, $value ) {
+			switch( $name ) {
+				case 'id':
+				case 'name':
+					throw new Exception( 'Cannot set ' . $name . ' property' );
+					break;
+				case 'modes':
+				case 'topic':
+					$this->update( $name, $value );
+					break;
+				default:
+					throw new Exception( 'Unknown property.' );
+			}
+			
+		}
+		
+		protected static function __get( $name ) {
+			switch( $name ) {
+				case 'id':
+					return $this->id;
+					break;
+				case 'name':
+					return $this->name;
+					break;
+				case 'modes':
+					return $this->modes;
+					break;
+				case 'topic':
+					return $this->topic;
+					break;
+				default:
+					return null;
+					break;
+			}
+		}
+			
+		protected function update( $name, $value ) {
+			MySQL::sql( 'UPDATE `access` SET `' . $name . '` = ' . MySQL::escape( $value ) . ' WHERE `id` = ' . MySQL::escape( $this->id ) );
+			$this->$name = $value;
+		}
+?>
