eb060163641f70efee1ad8e8811540b36a2ac299
snofox <snofox@cb14c9df-8a02-4fc1-9b40-3ccfc4edb2c1>
2010-03-07 21:13:24 +0000

    Fixed warning in ChannelUsers.php
    git-svn-id: https://cobihome.external.cluenet.org:8443/svn/phpserv/trunk@235 cb14c9df-8a02-4fc1-9b40-3ccfc4edb2c1

--- includes/ChannelUsers.php
+++ includes/ChannelUsers.php
@@ -24,7 +24,7 @@
 			$this->chanObj = $chanObj;
 		}
 		
-		protected function __get( $name ) {
+		public function __get( $name ) {
 			switch( $name ) {
 				case 'asArray':
 					return $this->userList;
